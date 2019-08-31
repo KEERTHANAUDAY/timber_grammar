db59f85ac2c2b0918e6c22dfcd833a497fa6300c
yck011522 <yck011522@gmail.com>
2019-08-31 14:14:34 +0200

    Archive Folder Removed

--- src/timber_grammar/Archive/20190815_test_for_victor.py
+++
@@ -1,4 +0,0 @@
-from assembly_model import Model
-
-test = Model.from_json("create_beam.json")
-print(test.data)
--- src/timber_grammar/Archive/20190820_Create_90Lap/20190817_test_load_from90lap.py
+++
@@ -1,6 +0,0 @@
-from assembly_model import Model
-
-m = Model()
-test = m.from_json("check_1.json")
-print(test)
-
