8ad93a0911e85e5f2ddd5fdd2fed636fcbcecb26
KEERTHANAUDAY <kudaykum@student.ethz.ch>
2019-09-09 11:43:47 +0200

    Added multiple meshes boolean feature  to trimesh proxy call.

--- src/timber_grammar/Trimesh_proxy/boolean.py
+++ src/timber_grammar/Trimesh_proxy/boolean.py
@@ -25,6 +25,38 @@ def trimesh_subtract(c_mesh1,c_mesh2):
     result_compas_mesh = Mesh.from_vertices_and_faces(boolean_sub.vertices, boolean_sub.faces)
     return result_compas_mesh
 
+def trimesh_subtract_multiple(meshes):
+    import compas
+    from compas.datastructures import Mesh
+    import trimesh
+    #import trimesh.boolean.difference as difference
+    
+    #Create a list of Trimesh objects from compas mesh objects
+    tri_meshes = []
+    for mesh in meshes:
+        assert isinstance(mesh,Mesh)
+        vertices = mesh.to_vertices_and_faces()[0]
+        faces = mesh.to_vertices_and_faces()[1]
+        new_trimesh = trimesh.Trimesh(vertices=vertices, faces=faces, process=False)
+        tri_meshes.append(new_trimesh)
+
+    # mesh1_v = c_mesh1.to_vertices_and_faces()[0]
+    # mesh1_f = c_mesh1.to_vertices_and_faces()[1]  
+
+    # mesh2_v = c_mesh2.to_vertices_and_faces()[0]
+    # mesh2_f = c_mesh2.to_vertices_and_faces()[1]
+
+    # mesh_1 = trimesh.Trimesh(vertices=mesh1_v, faces=mesh1_f, process=False)
+    # mesh_2 = trimesh.Trimesh(vertices=mesh2_v, faces=mesh2_f, process=False)
+    
+    # boolean_sub = mesh_1.difference(mesh_2,engine='scad')
+
+    boolean_sub =  trimesh.boolean.difference(tri_meshes,engine='scad')
+    # boolean_sub = tri_meshes[0].difference(tri_meshes[1:],engine='scad')
+
+    result_compas_mesh = Mesh.from_vertices_and_faces(boolean_sub.vertices, boolean_sub.faces)
+    return result_compas_mesh
+
 
 if __name__ == '__main__':
 
@@ -40,6 +72,13 @@ if __name__ == '__main__':
     box_2 = Box(([250,20,20], [300,0,100], [0,100,0]), 100, 50, 80)
     box_mesh_2 = Mesh.from_vertices_and_faces(box_2.vertices, box_2.faces)
 
-    print(trimesh_subtract(box_mesh, box_mesh_2))
+    result_1 = trimesh_subtract(box_mesh, box_mesh_2)
+    result_2 = trimesh_subtract_multiple([box_mesh, box_mesh_2])
+    
+    print("Comparing two boolean result") 
 
+    if (result_1.data == result_2.data):
+        print("Correct") 
+    else:
+        print("Incorrect")
 
