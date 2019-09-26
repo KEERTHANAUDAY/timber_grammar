faf2acd72fa8cc6475687aa3431b818d8b6b92e6
KEERTHANAUDAY <kudaykum@student.ethz.ch>
2019-09-26 16:35:02 +0200

    api_update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src/timber_grammar'))
 
+autodoc_mock_imports = ['Rhino', 'System']
 
 # -- Project information -----------------------------------------------------
 
 project = 'Timber_Grammar'
 copyright = '2019, Keerthana Udaykumar, Victor Leung'
-author = 'Keerthana Udaykumar, Victor Leung'
+author = 'Keerthana Udaykumar'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,15 +29,27 @@ author = 'Keerthana Udaykumar, Victor Leung'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,4 +64,32 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-master_doc = 'index'
+# Custom sidebar templates, must be a dictionary that maps document names
+# to template names.
+#
+# The default sidebars (for documents that don't match any pattern) are
+# defined by theme itself.  Builtin themes are using these templates by
+# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
+# 'searchbox.html']``.
+#
+# html_sidebars = {}
+
+html_use_modindex = True
+
+autodoc_member_order = 'bysource'
+# -- Options for HTMLHelp output ---------------------------------------------
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'pydoc'
+
+# -- Options for manual page output ------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'timber_grammarDocumentation', u'timber_grammar Documentation',
+     [author], 1)
+]
+
+
+
